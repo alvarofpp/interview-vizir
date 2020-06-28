1406d24f4a16e49445f899d2b38153f89a37033a
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-06-28 06:44:11 -0300

    Template Material UI

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './pages/Home/Home';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Livros from "./pages/Livros/Livros";
+import Autores from "./pages/Autores/Autores";
+import Sobre from "./pages/Sobre/Sobre";
+import NotFound from "./pages/NotFound/NotFound";
+import LandingPage from "./views/LandingPage/LandingPage";
+import ProfilePage from "./views/ProfilePage/ProfilePage";
+import LoginPage from "./views/LoginPage/LoginPage";
+import Components from "./views/Components/Components";
+
+import "./assets/scss/material-kit-react.scss?v=1.9.0";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
+      <Route component={NotFound}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
